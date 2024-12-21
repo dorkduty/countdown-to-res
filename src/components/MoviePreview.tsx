1b7344229bbdb6fd62e04392025e75eeea26b223
dorkduty <reggie@dorkduty.com>
2024-12-21 11:01:06 -0500

    Update MoviePreview.tsx
    update vimeo link

--- src/components/MoviePreview.tsx
+++ src/components/MoviePreview.tsx
@@ -18,7 +18,7 @@ export const MoviePreview = () => {
             <div className="aspect-video rounded-lg overflow-hidden shadow-2xl">
               <div style={{ padding: '56.25% 0 0 0', position: 'relative' }}>
                 <iframe 
-                  src="https://player.vimeo.com/video/1041241030h=3f4d4cb099&badge=0&autopause=0&player_id=0&app_id=58479&title=0"
+                  src="https://player.vimeo.com/video/1041241030?h=1a111058d8&badge=0&autopause=0&player_id=0&app_id=58479&title=0"
                   style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
                   frameBorder="0"
                   allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
