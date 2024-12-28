ffbf155f9f8faf049f31f7bbbc2263aef494fdf0
dorkduty <reggie@dorkduty.com>
2024-12-27 21:09:14 -0500

    Update MoviePreview.tsx
    remove youtube related videos

--- src/components/MoviePreview.tsx
+++ src/components/MoviePreview.tsx
@@ -20,7 +20,7 @@ export const MoviePreview = () => {
                 <iframe 
                   width="100%" 
                   height="100%" 
-                  src="https://www.youtube.com/embed/IwXamRdHSog?si=xeyKzUnqM41CWyVe?rel=0" 
+                  src="https://www.youtube.com/embed/IwXamRdHSog?si=xeyKzUnqM41CWyVe&rel=0" 
                   title="YouTube video player" 
                   frameBorder="0" 
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
