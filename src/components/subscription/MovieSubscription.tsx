45b7d243c7a66b01ec4cd1210aac2b4e193a719e
dorkduty <reggie@dorkduty.com>
2024-12-29 18:19:45 -0500

    Update MovieSubscription.tsx
    like of mike move poster

--- src/components/subscription/MovieSubscription.tsx
+++ src/components/subscription/MovieSubscription.tsx
@@ -16,7 +16,7 @@ export const MovieSubscription = ({
   <div className="p-6 bg-primary">
     <div className="flex flex-col items-center space-y-4">
       <img
-        src="/lovable-uploads/dc48ccce-2d8f-4366-9d9c-44da187a43c6.png"
+        src="https://3a5ae66a6e58ba5f79caf8a72e703216.cdn.bubble.io/f1735514244512x915430778266064500/life%20of%20mike%20logo%20in%20theaters.png?_gl=1*aq74u8*_gcl_au*ODgzNzc2NDIyLjE3MzUwODQwODM.*_ga*MTY2MzU2NjI1LjE3MzQ0ODQ1MDI.*_ga_BFPVR2DEE2*MTczNTQ4ODgzNS43LjEuMTczNTUxNDIyOS40Mi4wLjA."
         alt="Life of Mike Movie Poster"
         className="w-48 h-48 object-cover rounded-lg shadow-lg"
       />
